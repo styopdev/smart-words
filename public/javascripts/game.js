42cf65a4eaeefb70fbfedefac38dd19f581183fd
hmatevosyan <h.matevosyan@yahoo.com>
2015-05-02 03:56:16 -0700

    "Remove Hint" & "Skip Question" scripts

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -43,4 +43,37 @@ $(document).ready(function(){
     // Schedule the update to happen once every second
     var interval = setInterval(doUpdate, 5000);
 
+    // remove wrong answer
+    $('#remove-wrong-answer').click(function(){
+
+        var wrongAnswers = [];
+        $(".choose-answers .wrong").each(function() {
+            var answers = $(this).attr("id");
+            wrongAnswers.push(answers);  // push wrong answers into array
+        }); // elements in this class are being pushed into the array 'cCameras'
+
+        // generate random number within the length of array's range
+        var randomAnswer = Math.floor(Math.random() * wrongAnswers.length);
+        console.log(wrongAnswers[randomAnswer]);
+       // wrongAnswers[randomAnswer].css({'background':'#3DCC01', 'border-color':'#FEFB64'});
+
+        $('#' + wrongAnswers[randomAnswer]).css({'background':'#A8A8A8', 'color':'#FFFFFF', 'border-color':'#000000', 'pointer-events': 'none'});
+        $('#' + wrongAnswers[randomAnswer]).click (function () {
+            return false;
+        });
+
+     /*   $('#' + wrongAnswers[randomAnswer]).click(function(){
+
+            console.log(this);
+        });
+
+        $('#answer3').click(function(){
+
+            console.log(this);
+        }); */
+       // $('.my-link').bind('click', false);
+        // for (var i = 0; i < wrongAnswers.length; i++)
+        //     console.log(wrongAnswers[i]);
+    });
+
 });// complete click
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
     // Audio Player
     var playing = false;
 
@@ -17,32 +16,44 @@ $(document).ready(function(){
 
 
     // Progress bar
+    $('#skip-question').click(function(){
+        var currentProgress = $('#progress-bar').val();  // get current progress value
+        var progressStep = 20;
+        console.log($('#progress-bar').val());
+        $('#progress-bar').val(currentProgress + progressStep); // shift one step forward
+        /*
+        if($('.bar span').hasClass('border-change')){
+            $('#progress-bar').val(progressStep);
+        } */
+    });
+
     $('.bar span').click(function(){
+        var progressStep = 20;
         if ($(this).hasClass('first')){
-            $('#progress-bar').val('10');
+            $('#progress-bar').val(progressStep);
             $(this).nextAll().removeClass('border-change');
             $('.percent').html("20% Complete");
         }else if ($(this).hasClass('second')){
             $(this).nextAll().removeClass('border-change');
-            $('#progress-bar').val('32');
+            $('#progress-bar').val(progressStep * 2);
             $(this).prevAll().addClass('border-change');
             $(this).addClass('border-change');
             $('.percent').html("40% Complete");
         }else if ($(this).hasClass('third')){
             $(this).nextAll().removeClass('border-change');
-            $('#progress-bar').val('50');
+            $('#progress-bar').val(progressStep * 3);
             $(this).prevAll().addClass('border-change');
             $(this).addClass('border-change');
             $('.percent').html("60% Complete");
         }else if ($(this).hasClass('fourth')){
             $(this).nextAll().removeClass('border-change');
-            $('#progress-bar').val('70');
+            $('#progress-bar').val(progressStep * 4);
             $(this).prevAll().addClass('border-change');
             $(this).addClass('border-change');
             $('.percent').html("80% Complete");
         }else if ($(this).hasClass('fifth')){
             $(this).nextAll().removeClass('border-change');
-            $('#progress-bar').val('100');
+            $('#progress-bar').val(progressStep * 5);
             $(this).prevAll().addClass('border-change');
             $(this).addClass('border-change');
             $('.percent').html("100% Complete");
