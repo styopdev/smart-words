8fed019cb4d9c2ad3fb97388f28adebaae2016a6
Stepan <styopdev@hotmail.com>
2018-02-15 15:42:21 +0400

    Update login.js

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -66,7 +66,7 @@ window.fbAsyncInit = function() {
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
-    FB.api('/me', { fields: 'name, email, gender, locale, location' }, function(response) {
+    FB.api('/me', { fields: 'name, email, gender, locale, user_location' }, function(response) {
         if (response.name && response.email) {
             $.ajax({
                 type : "POST",
